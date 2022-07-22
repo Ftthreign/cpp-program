6b3caee84c3463b5315a2676eeea0e2bc361f102
Ftthreign <98510825+Ftthreign@users.noreply.github.com>
2022-07-22 18:59:12 +0700

    Add files via upload

---
+++ untitled.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "halo dunia" << std::endl;
+    std::cin.get();
+    // jika tidak ingin menggunakan std:: pada kodingan maka dapat menggukan using namespace std; dibawah #include <iostream>
+    return 0;
+}
